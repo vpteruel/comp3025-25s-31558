57794a785c1bc983990fef7c76e6713ce68e52d0
Vinicius Teruel <vpteruel@gmail.com>
2025-08-09 22:03:26 -0400

    refactor: update Kotlin compiler options to target JVM 11

--- final-assignment/app/build.gradle.kts
+++ final-assignment/app/build.gradle.kts
@@ -43,8 +43,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
     }
     buildFeatures {
         viewBinding = true
